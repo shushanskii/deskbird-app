8005888e3c739a0fabfb5a5af169d1b6d835854f
Konstantin Shushanskii <konstantin@shushanskii.com>
2024-09-06 22:07:34 +0200

    feat: add login forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core'
+import { Component, signal } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatSelectModule } from '@angular/material/select'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIcon } from '@angular/material/icon'
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIcon,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  hide = signal(true)
+
+  passwordErrorMessage = signal('')
+  loginErrorMessage = signal('')
+
+  loginForm = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  })
+
+  onEyeClick(event: MouseEvent) {
+    this.hide.set(!this.hide())
+    event.stopPropagation()
+  }
+
+  onPasswordBlur() {
+    const passwordControl = this.loginForm.get('password')
+    if (passwordControl?.hasError('required')) {
+      this.passwordErrorMessage.set('Password is required')
+    } else {
+      this.passwordErrorMessage.set('')
+    }
+  }
+
+  onLoginBlur() {
+    const loginControl = this.loginForm.get('login')
+    if (loginControl?.hasError('required')) {
+      this.loginErrorMessage.set('Login is required')
+    } else {
+      this.loginErrorMessage.set('')
+    }
+  }
+
+  onSubmit() {
+    console.info(this.loginForm.value)
+  }
+}
