512af265dc7c37ccf5e296d419578a236fd8c0c1
Konstantin Shushanskii <konstantin@shushanskii.com>
2024-09-06 20:56:18 +0200

    feat: add users list table

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,11 +2,36 @@ import { Component, Input, OnInit } from '@angular/core'
 import { Observable, of } from 'rxjs'
 import { AsyncPipe, NgFor } from '@angular/common'
 import { User, UsersStore } from 'store/users.store'
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+} from '@angular/material/table'
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [NgFor, AsyncPipe],
+  imports: [
+    NgFor,
+    AsyncPipe,
+    MatTable,
+    MatHeaderCell,
+    MatColumnDef,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef,
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
   providers: [UsersStore],
@@ -16,6 +41,8 @@ export class UsersComponent implements OnInit {
   userId: string | undefined = undefined
 
   users$: Observable<User[]> = of([])
+
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email']
 
   constructor(private readonly usersStore: UsersStore) {}
 
